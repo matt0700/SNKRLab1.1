11f1fa11fa19ce9b640c0304e6c9f92f99022551
LloydAllenArguelles <allen.arguelles27@gmail.com>
2023-12-19 00:01:42 +0800

    added here laravel

---
+++ SNKRLAB/resources/views/homepage.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Blank Blade Page</title>
+</head>
+<body>
+    <h1>Hello, this is a blank Blade template!</h1>
+    <!-- Add your Blade content here -->
+</body>
+</html>
